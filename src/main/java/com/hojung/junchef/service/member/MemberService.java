b2a7745662977111844fddf73f96d4e1f9b14530
jjun <hjs8236@gmail.com>
2023-09-04 16:23:24 +0900

    Feat: MemberService 구현

--- src/main/java/com/hojung/junchef/service/member/MemberService.java
+++ src/main/java/com/hojung/junchef/service/member/MemberService.java
@@ -1,9 +1,73 @@
 package com.hojung.junchef.service.member;
 
+import com.hojung.junchef.domain.member.Member;
+import com.hojung.junchef.repository.member.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+
+        return member.getId();
+    }
+
+    public void validateDuplicateMember(Member member) {
+        memberRepository.findByEmail(member.getEmail())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("중복 회원");
+                });
+    }
+
+    public List<Member> findAll() {
+        return memberRepository.findAll();
+    }
+
+    public Member findById(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(
+                        () -> new IllegalStateException("없는 회원")
+                );
+    }
+
+    public void changePassword(Long memberId, String newPassword) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(
+                        () -> new IllegalStateException("없는 회원")
+                );
+        member.setPasswd(newPassword);
+
+        memberRepository.save(member);
+    }
+
+    public void delete(Long memberId) {
+        memberRepository.deleteById(memberId);
+    }
+
+    public Long login(String email, String passwd) {
+        Member member = memberRepository.findByEmail(email)
+                .orElseThrow(
+                        () -> new IllegalStateException("존재하지 않는 아이디")
+                );
+
+        if (!member.getPasswd().equals(passwd)) {
+            throw new IllegalStateException("비밀번호가 일치하지 않음");
+        }
+
+        return member.getId();
+    }
 }

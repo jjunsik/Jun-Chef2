55bbf324bca7baed2608cdb7bde624e27149edda
jjun <hjs8236@gmail.com>
2023-09-04 16:14:16 +0900

    Feat: MemberRepository 구현

---
+++ src/main/java/com/hojung/junchef/repository/member/MemberRepositoryImpl.java
@@ -0,0 +1,59 @@
+package com.hojung.junchef.repository.member;
+
+import com.hojung.junchef.domain.member.Member;
+import jakarta.persistence.EntityManager;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class MemberRepositoryImpl implements MemberRepository {
+    private final EntityManager em;
+
+    public MemberRepositoryImpl(EntityManager em) {
+        this.em = em;
+    }
+
+    @Override
+    public Member save(Member member) {
+        em.persist(member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+
+        return Optional.ofNullable(member);
+    }
+
+    @Override
+    public Optional<Member> findByEmail(String email) {
+        List<Member> result = em.createQuery("select m from Member m where m.email = :email", Member.class)
+                .setParameter("email", email)
+                .getResultList();
+
+        return result.stream().findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        Member member = em.find(Member.class, id);
+        if(member != null)
+            deleteById(member);
+    }
+
+    @Override
+    public void deleteById(Member member) {
+        em.remove(member);
+        em.flush();
+        em.clear();
+    }
+}

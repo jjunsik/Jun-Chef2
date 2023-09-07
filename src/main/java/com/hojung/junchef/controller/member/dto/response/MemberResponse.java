01118696d22c987cba2cc83bf7cb9dd55f0152df
jjunsik <junsik8236@gmail.com>
2023-09-07 14:49:43 +0900

    Feat: 회원에 관한 처리를 정확히 수행했는지 확인하기 위한 Response 객체 구현

---
+++ src/main/java/com/hojung/junchef/controller/member/dto/response/MemberResponse.java
@@ -0,0 +1,12 @@
+package com.hojung.junchef.controller.member.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberResponse {
+    private Long memberId;
+}

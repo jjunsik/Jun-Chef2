8c8561e547b3fd49af3b6651fd0cafda274610cd
jjunsik <junsik8236@gmail.com>
2023-09-08 13:47:35 +0900

    Feat: MemberController 구현

--- src/main/java/com/hojung/junchef/controller/member/MemberController.java
+++ src/main/java/com/hojung/junchef/controller/member/MemberController.java
@@ -1,9 +1,71 @@
 package com.hojung.junchef.controller.member;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.hojung.junchef.controller.member.dto.request.ChangePasswordRequest;
+import com.hojung.junchef.controller.member.dto.request.LoginRequest;
+import com.hojung.junchef.controller.member.dto.request.MemberJoinRequest;
+import com.hojung.junchef.controller.member.dto.response.GetMemberResponse;
+import com.hojung.junchef.controller.member.dto.response.MemberResponse;
+import com.hojung.junchef.domain.member.Member;
+import com.hojung.junchef.service.member.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("jun-chef/v1/members")
 public class MemberController {
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping
+    public List<Member> getAllMembers() {
+        return memberService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public GetMemberResponse getMember(@PathVariable("id") Long memberId) {
+        Member member = memberService.findById(memberId);
+
+        return new GetMemberResponse(member);
+    }
+
+    @PostMapping
+    public MemberResponse join(@RequestBody MemberJoinRequest joinRequest) {
+        Long memberId = memberService.join(joinRequest.toMember());
+
+        return new MemberResponse(memberId);
+    }
+
+    @PutMapping("/{id}")
+    public MemberResponse changePassword(@PathVariable("id") Long memberId, @RequestBody ChangePasswordRequest changePasswordRequest) {
+        memberService.changePassword(memberId, changePasswordRequest.getNewPasswd());
+
+        return new MemberResponse(memberId);
+    }
+
+    @DeleteMapping("/{id}")
+    public MemberResponse delete(@PathVariable("id") Long memberId) {
+        memberService.delete(memberId);
+
+        return new MemberResponse(memberId);
+    }
+
+    @PostMapping("/login")
+    public MemberResponse login(@RequestBody LoginRequest loginRequest) {
+        Long memberId = memberService.login(loginRequest.getEmail(), loginRequest.getPasswd());
+
+        return new MemberResponse(memberId);
+    }
+
+//    @PostMapping("/logout")
+//    public MemberResponse logout(@RequestBody LoginRequest loginRequest) {
+//        Long memberId = memberService.login(loginRequest.getEmail(), loginRequest.getPasswd());
+//
+//        return new MemberResponse(memberId);
+//    }
 }

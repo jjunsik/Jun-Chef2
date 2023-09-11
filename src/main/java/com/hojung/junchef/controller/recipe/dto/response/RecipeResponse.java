13739f1e0d7e54575ba668b9bc4a930128da79a4
jjunsik <junsik8236@gmail.com>
2023-09-11 13:59:07 +0900

    Feat: 레시피를 조회를 제외한 다른 행동을 할 때 id 값을 반환하도록 객체 구현

---
+++ src/main/java/com/hojung/junchef/controller/recipe/dto/response/RecipeResponse.java
@@ -0,0 +1,12 @@
+package com.hojung.junchef.controller.recipe.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RecipeResponse {
+    private Long recipeId;
+}

11510926b258b4ada6f3722b847b76602d7b59d1
jjunsik <junsik8236@gmail.com>
2023-09-12 15:37:45 +0900

    Simplify: @Data 어노테이션이 아닌 @Getter 어노테이션을 추가하여 필요한 기능만 하도록 변경

--- src/main/java/com/hojung/junchef/controller/recipe/dto/response/RecipeResponse.java
+++ src/main/java/com/hojung/junchef/controller/recipe/dto/response/RecipeResponse.java
@@ -1,10 +1,10 @@
 package com.hojung.junchef.controller.recipe.dto.response;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Data
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class RecipeResponse {

6076861e180d2dee1e1cb03642c0c72006fe0ded
jjunsik <junsik8236@gmail.com>
2023-09-13 15:07:49 +0900

    Feat: JPA Auditing 을 활성화 시키기 위해 AuditingConfig 클래스 추가

---
+++ src/main/java/com/hojung/junchef/util/config/AuditingConfig.java
@@ -0,0 +1,9 @@
+package com.hojung.junchef.util.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing // JPA Auditing 활성화
+public class AuditingConfig {
+}

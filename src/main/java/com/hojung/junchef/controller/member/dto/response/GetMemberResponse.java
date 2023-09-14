a6cabcdd2765a45e03b6cbb889ed41651e786a7c
jjunsik <junsik8236@gmail.com>
2023-09-14 14:22:28 +0900

    Refactor: Member의 History를 수정 시간에 대해 내림차순으로 정렬 후 반환하도록 변경

--- src/main/java/com/hojung/junchef/controller/member/dto/response/GetMemberResponse.java
+++ src/main/java/com/hojung/junchef/controller/member/dto/response/GetMemberResponse.java
@@ -1,20 +1,31 @@
 package com.hojung.junchef.controller.member.dto.response;
 
+import com.hojung.junchef.controller.history.dto.response.GetHistoryResponse;
 import com.hojung.junchef.domain.history.History;
 import com.hojung.junchef.domain.member.Member;
 import lombok.Data;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 public class GetMemberResponse {
     private final String name;
     private final String email;
-    private final List<History> history;
+    private final List<GetHistoryResponse> histories;
 
     public GetMemberResponse(Member member) {
         this.name = member.getName();
         this.email = member.getEmail();
-        this.history = member.getHistory();
+
+        List<History> memberHistories = member.getHistories();
+
+        // 내림차순으로 정렬
+        memberHistories.sort(Comparator.comparing(History::getLastModifiedDateTime).reversed());
+
+        this.histories = member.getHistories().stream()
+                .map(GetHistoryResponse::of)
+                .collect(Collectors.toList());
     }
 }

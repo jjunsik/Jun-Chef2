c3a7843ecd13282853aff46376893427c49b531d
jjunsik <junsik8236@gmail.com>
2023-09-14 14:23:35 +0900

    Refactor: 연관관계 편의 메서드와 수정 시간에 대한 getter 삭제, Recipe 객체와 OneToOne에서 ManyToOne으로 변경

--- src/main/java/com/hojung/junchef/domain/history/History.java
+++ src/main/java/com/hojung/junchef/domain/history/History.java
@@ -8,10 +8,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.format.DateTimeFormatter;
-
-import static com.hojung.junchef.domain.constant.HistoryConstant.DATE_TIME_FORMAT;
-
 @Entity
 @Getter
 @Setter
@@ -24,21 +20,12 @@ public class History extends BaseEntity {
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "recipe_id")
     private Recipe recipe;
 
     public History(Member member, Recipe recipe) {
         this.member = member;
         this.recipe = recipe;
-
-        if(member != null){
-            member.getHistory().add(0, this);
-        }
-    }
-
-    public String getDateTime() {
-        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern(DATE_TIME_FORMAT);
-        return this.lastModifiedDateTime.format(timeFormatter);
     }
 }

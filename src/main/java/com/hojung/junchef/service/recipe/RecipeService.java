95778879f3fa6241128704b87f8d2a7b80872aa2
jjunsik <junsik8236@gmail.com>
2023-09-15 17:05:48 +0900

    Improve: 같은 음식 이름을 공백이 있는 상태로 검색하여도 새로 검색하지 않고 Repository 에 있는 레시피를 조회하도록 하기 위해 검색어에 대한 공백을 모두 제거한 상태로 RecipeRepository 에 저장함과 동시에 HistoryRepository 에 최근 검색어로 저장

--- src/main/java/com/hojung/junchef/service/recipe/RecipeService.java
+++ src/main/java/com/hojung/junchef/service/recipe/RecipeService.java
@@ -14,29 +14,48 @@ import static com.hojung.junchef.service.constant.RecipeServiceConstant.NON_EXIS
 public class RecipeService {
     private final RecipeRepository recipeRepository;
     private final RecipeGptService recipeGptService;
+    private final MemberService memberService;
+    private final HistoryService historyService;
 
-    String nonExistRecipeErrorMsg = "없는 레시피";
+    public Recipe findByName(Long memberId, String recipeName) {
+        Member member = memberService.findById(memberId);
 
-    public Recipe findByName(String recipeName) {
-        return recipeRepository.findByName(recipeName)
+        // RecipeRepository 에 저장할 때, 무조건 공백이 제거된 상태로 저장했기 때문에 replaceAll 을 사용
+        String replaceRecipeName = recipeName.replaceAll("\\s+", "");
+
+        Recipe recipe = recipeRepository.findByName(replaceRecipeName)
                 .orElseGet(
                         () -> getRecipeFromChatGPT(recipeName)
                 );
+
+        // 사용자는 항상 공백이 제거된 음식의 이름이 최근 검색어 목록으로 저장이 됨.
+        History history = new History(member, recipe);
+        historyService.save(history);
+
+        return recipe;
     }
 
     // findByName() 처럼 gpt 와 통신을 해서 레시피를 얻어와야 하는지 아직은 모르겠음.
-    public Recipe findById(Long recipeId) {
-        return recipeRepository.findById(recipeId)
+    public Recipe findById(Long memberId, Long recipeId) {
+        Member member = memberService.findById(memberId);
+
+        Recipe recipe = recipeRepository.findById(recipeId)
                 .orElseThrow(
                         () -> new IllegalStateException(NON_EXIST_RECIPE_ERROR_MESSAGE)
                 );
+
+        History history = new History(member, recipe);
+        historyService.save(history);
+
+        return recipe;
     }
 
     private Recipe getRecipeFromChatGPT(String recipeName) {
         String result = recipeGptService.search(recipeName);
 
+        // RecipeRepository 에 Recipe 를 저장할 때, 무조건 음식 이름의 공백을 모두 제거하고 저장하기 위해 replaceAll 을 사용
         Recipe recipe = Recipe.builder()
-                .recipeName(recipeName)
+                .recipeName(recipeName.replaceAll("\\s+", ""))
                 .result(result)
                 .build();
 

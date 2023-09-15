d62917b561f0d154b66780cb4faa072844e49398
jjunsik <junsik8236@gmail.com>
2023-09-15 17:08:08 +0900

    Feat: RecipeRepository 구현

---
+++ src/main/java/com/hojung/junchef/repository/recipe/RecipeRepositoryImpl.java
@@ -0,0 +1,40 @@
+package com.hojung.junchef.repository.recipe;
+
+import com.hojung.junchef.domain.recipe.Recipe;
+import jakarta.persistence.EntityManager;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class RecipeRepositoryImpl implements RecipeRepository {
+    private final EntityManager em;
+
+    public RecipeRepositoryImpl(EntityManager em) {
+        this.em = em;
+    }
+
+    @Override
+    public Recipe save(Recipe recipe) {
+        em.persist(recipe);
+
+        return recipe;
+    }
+
+    @Override
+    public Optional<Recipe> findById(Long id) {
+        Recipe recipe = em.find(Recipe.class, id);
+
+        return Optional.ofNullable(recipe);
+    }
+
+    @Override
+    public Optional<Recipe> findByName(String name) {
+        List<Recipe> result = em.createQuery("select r from Recipe r where r.recipeName = :recipeName", Recipe.class)
+                .setParameter("recipeName", name)
+                .getResultList();
+
+        return result.stream().findAny();
+    }
+}

ed0fff9a8ee72bff48d067a3618a606f936270f3
jjunsik <junsik8236@gmail.com>
2023-09-16 14:10:27 +0900

    Refactor: HistoryService 의 상수화 필드를 클래스로 추출

---
+++ src/main/java/com/hojung/junchef/service/constant/HistoryServiceConstant.java
@@ -0,0 +1,6 @@
+package com.hojung.junchef.service.constant;
+
+public class HistoryServiceConstant {
+    public static final int HISTORY_MAX_SIZE = 5;
+    public static final int DELETE_MEMBER_HISTORY_INDEX = 0;
+}

6b3ca6dde59b87fa7fcb75cb3fb8bfc42b8e33d8
jjunsik <junsik8236@gmail.com>
2023-10-05 18:43:03 +0900

    Simplify: API KEY 노출 안 하도록 변경

--- src/main/java/com/hojung/junchef/util/http/request/ChatGptRequest.java
+++ src/main/java/com/hojung/junchef/util/http/request/ChatGptRequest.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 public class ChatGptRequest {
-    private static final String CHAT_GPT_KEY = "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";
+    private static final String CHAT_GPT_KEY = "KEY";
     private static final String CHAT_GPT_URL = "https://api.openai.com/v1/chat/completions";
     private static final String CHAT_GPT_MODEL = "gpt-3.5-turbo";
     private final List<String> messages;
